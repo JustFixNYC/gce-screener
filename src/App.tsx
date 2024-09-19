b0d9609d9657a66ec2fe6b18f48c2dbe1d44a649
Steve Giordano <steve1nyc@gmail.com>
2024-09-19 16:17:12 -0400

    Add routing and two pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,33 @@
-import { Button } from "@justfixnyc/component-library";
 import "./App.scss";
-import { GeoSearchInput } from "./Components/GeoSearchInput/GeoSearchInput";
+import { Routes, Route, Outlet } from "react-router";
+import { Home } from "./Components/Pages/Home/Home";
+import { Form } from "./Components/Pages/Form/Form";
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="form" element={<Form />} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
+
+const Layout = () => {
   return (
     <div id="container">
       <header id="header">
         <h1>Good Cause Eviction</h1>
       </header>
+
       <div id="main">
-        <h2>What is Good Cause Eviction?</h2>
-        <p className="content-p">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tellus
-          lacus, pharetra dictum bibendum eu, ornare non mauris. Fusce pretium
-          laoreet magna, ac dictum enim dictum sit amet. Vivamus sit amet augue
-          ut metus auctor eleifend. Mauris tincidunt auctor lorem a ultricies.
-          Aenean enim magna, semper ac arcu volutpat, mattis convallis nibh.
-          Mauris efficitur augue quam, ut pharetra felis vehicula sed. In hac
-          habitasse platea dictumst. Pellentesque eget cursus neque, in
-          pellentesque mi.
-        </p>
-
-
-        <div className="geo-search-form">
-          <GeoSearchInput />
-          <Button labelText="See if you are eligible" size="small"/>
+        <div id="content">
+          <Outlet />
         </div>
       </div>
-      <footer id="footer">
-        <p className="legal-header">Legal Disclaimer</p>
-        <p className="content-p">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tellus
-          lacus, pharetra dictum bibendum eu, ornare non mauris. Fusce pretium
-          laoreet magna, ac dictum enim dictum sit amet. Vivamus sit amet augue
-          ut metus auctor eleifend. Mauris tincidunt auctor lorem a ultricies.
-          Aenean enim magna, semper ac arcu volutpat, mattis convallis nibh.
-          Mauris efficitur augue quam, ut pharetra felis vehicula sed. In hac
-          habitasse platea dictumst. Pellentesque eget cursus neque, in
-          pellentesque mi.
-        </p>
-      </footer>
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/Components/Pages/Home/Home.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@justfixnyc/component-library";
+import { GeoSearchInput } from "../../GeoSearchInput/GeoSearchInput";
+import "./Home.scss";
+
+export const Home = () => {
+  return (
+    <div className="wrapper">
+      <div className="main-content">
+        <h2>What is Good Cause Eviction?</h2>
+        <p className="content-p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tellus
+          lacus, pharetra dictum bibendum eu, ornare non mauris. Fusce pretium
+          laoreet magna, ac dictum enim dictum sit amet. Vivamus sit amet augue
+          ut metus auctor eleifend. Mauris tincidunt auctor lorem a ultricies.
+          Aenean enim magna, semper ac arcu volutpat, mattis convallis nibh.
+          Mauris efficitur augue quam, ut pharetra felis vehicula sed. In hac
+          habitasse platea dictumst. Pellentesque eget cursus neque, in
+          pellentesque mi.
+        </p>
+
+        <div className="geo-search-form">
+          <GeoSearchInput />
+          <Button labelText="See if you are eligible" size="small" />
+        </div>
+      </div>
+      <div id="legal-footer">
+        <p className="legal-header">Legal Disclaimer</p>
+        <p className="content-p">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tellus
+          lacus, pharetra dictum bibendum eu, ornare non mauris. Fusce pretium
+          laoreet magna, ac dictum enim dictum sit amet. Vivamus sit amet augue
+          ut metus auctor eleifend. Mauris tincidunt auctor lorem a ultricies.
+          Aenean enim magna, semper ac arcu volutpat, mattis convallis nibh.
+          Mauris efficitur augue quam, ut pharetra felis vehicula sed. In hac
+          habitasse platea dictumst. Pellentesque eget cursus neque, in
+          pellentesque mi.
+        </p>
+      </div>
+    </div>
+  );
+};
