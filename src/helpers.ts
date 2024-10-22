bf17fe7551ce6753c0ad7d4090f6e8c08d008f7f
Maxwell Austensen <m.austensen@gmail.com>
2024-10-22 17:55:20 -0400

    show acris data for related bbls

--- src/helpers.ts
+++ src/helpers.ts
@@ -37,3 +37,17 @@ export const getDetermination = (
     return "eligible";
   }
 };
+
+export const splitBBL = (
+  bbl: string
+): {
+  boro: string;
+  block: string;
+  lot: string;
+} => {
+  const bblArr = bbl.split("");
+  const boro = bblArr.slice(0, 1).join("");
+  const block = bblArr.slice(1, 6).join("");
+  const lot = bblArr.slice(6, 10).join("");
+  return { boro, block, lot };
+};
