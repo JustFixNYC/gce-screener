8d67548aacacaadb195a772104ad4ae20a43328e
Maxwell Austensen <m.austensen@gmail.com>
2024-10-22 17:54:36 -0400

    update api specs

--- src/types/APIDataTypes.ts
+++ src/types/APIDataTypes.ts
@@ -1,10 +1,16 @@
+type AcrisProperty = {
+  bbl: string,
+  doc_id: string,
+  doc_date: string,
+  unitsres: number,
+}
+
 export type BuildingEligibilityInfo = {
   bbl: string,
   unitsres: number,
   wow_portfolio_units: number,
   wow_portfolio_bbls: number,
   bldgclass: string,
-  bldgclass_desc: string,
   yearbuilt: number,
   latest_co: string,
   co_bin: string,
@@ -14,4 +20,5 @@ export type BuildingEligibilityInfo = {
   subsidy_name: string,
   end_421a: string,
   end_j51: string,
+  acris_data: AcrisProperty[] | null,
 }
