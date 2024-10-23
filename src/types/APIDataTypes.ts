0b5d170f5813192924f8d70b0fa5ca0adee929ba
Maxwell Austensen <m.austensen@gmail.com>
2024-10-23 15:48:22 -0400

    update api spec and types for wow_data

--- src/types/APIDataTypes.ts
+++ src/types/APIDataTypes.ts
@@ -5,6 +5,15 @@ type AcrisProperty = {
   unitsres: number,
 }
 
+type WowBuildings = {
+  bbl: string,
+  distance_m: number,
+  match_name: boolean,
+  match_bizaddr_unit: boolean,
+  match_bizaddr_nounit: boolean,
+  unitsres: number,
+}
+
 export type BuildingEligibilityInfo = {
   bbl: string,
   unitsres: number,
@@ -21,4 +30,5 @@ export type BuildingEligibilityInfo = {
   end_421a: string,
   end_j51: string,
   acris_data: AcrisProperty[] | null,
+  wow_data: WowBuildings[] | null,
 }
