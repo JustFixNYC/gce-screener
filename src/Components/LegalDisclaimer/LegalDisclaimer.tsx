eb51d4a49f98df22c939146cd4f5a648b5050c03
Steve Giordano <steve1nyc@gmail.com>
2024-10-25 16:12:03 -0400

    Make the legal header an h2

--- src/Components/LegalDisclaimer/LegalDisclaimer.tsx
+++ src/Components/LegalDisclaimer/LegalDisclaimer.tsx
@@ -1,7 +1,7 @@
 export const LegalDisclaimer: React.FC = () => {
   return (
     <div id="legal-footer">
-      <p className="legal-header">Legal Disclaimer</p>
+      <h2 className="legal-header">Legal Disclaimer</h2>
       <p className="content-p">
         The information on this website does not constitute legal advice and
         must not be used as a substitute for the advice of a lawyer qualified to
