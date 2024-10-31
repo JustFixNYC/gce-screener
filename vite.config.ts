0bdbc7b312d4d0bca5d1268f2ec9d7bbc8815a93
Steve Giordano <steve1nyc@gmail.com>
2024-10-31 12:47:24 -0400

    Compress build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,10 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression()
+  ],
 })
+
+
