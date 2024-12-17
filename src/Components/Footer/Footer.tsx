79fdae56202e440d0cbb8a097613ac5ee1473f25
Maxwell Austensen <maxwell@justfix.org>
2024-12-17 13:24:30 -0500

    remove page links from footer (#49)

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,19 +1,8 @@
-import { Link, To } from "react-router-dom";
 import "./Footer.scss";
-
-const SITE_LINKS: LinkWithLabel[] = [
-  { label: "Page 1", path: "/" },
-  { label: "Page 2", path: "/" },
-  { label: "Page 3", path: "/" },
-  { label: "Page 4", path: "/" },
-  { label: "Page 5", path: "/" },
-];
 
 export const Footer: React.FC = () => (
   <footer className="footer">
     <div className="footer__content">
-      <FooterLinksList links={SITE_LINKS} />
-      <div className="footer__divider" />
       <div className="footer__legal_disclaimer">
         Disclaimer: The information in JustFix does not constitute legal advice
         and must not be used as a substitute for the advice of a lawyer
@@ -41,28 +30,3 @@ export const Footer: React.FC = () => (
     </div>
   </footer>
 );
-
-type LinkWithLabel = { label: string; path: To };
-
-const FooterLink: React.FC<{ link: LinkWithLabel }> = ({ link }) => (
-  <Link to={link.path} className="jfcl-link">
-    {link.label}
-  </Link>
-);
-
-const FooterLinksList: React.FC<{ links: LinkWithLabel[] }> = ({ links }) => {
-  return (
-    <div className="footer__page_links">
-      {links.map((link, i) => {
-        if (i % 2 === 0) {
-          return (
-            <div className="footer__page_links__column" key={i}>
-              <FooterLink link={link} />
-              {!!links[i + 1] && <FooterLink link={links[i + 1]} />}
-            </div>
-          );
-        } else return;
-      })}
-    </div>
-  );
-};
