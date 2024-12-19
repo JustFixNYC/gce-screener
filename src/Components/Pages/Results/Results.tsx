403b7c8d12dad6b234edb24b14441d71bd2407ff
Maxwell Austensen <m.austensen@gmail.com>
2024-12-19 12:29:55 -0500

    change listeners from media change to before/after print

--- src/Components/Pages/Results/Results.tsx
+++ src/Components/Pages/Results/Results.tsx
@@ -71,31 +71,30 @@ export const Results: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handlePrintAccordions = (event: MediaQueryListEvent) => {
-    let accordions: NodeListOf<HTMLDetailsElement>;
-    if (event.matches) {
-      accordions = document.body.querySelectorAll("details:not([open])");
-      accordions.forEach((e) => {
-        e.setAttribute("open", "");
-        e.dataset.wasclosed = "";
-      });
-    } else {
-      accordions = document.body.querySelectorAll("details[data-wasclosed]");
-      accordions.forEach((e) => {
-        e.removeAttribute("open");
-        delete e.dataset.wasclosed;
-      });
-    }
+  const openAccordionsPrint = () => {
+    const accordions: NodeListOf<HTMLDetailsElement> =
+      document.body.querySelectorAll("details:not([open])");
+    accordions.forEach((e) => {
+      e.setAttribute("open", "");
+      e.dataset.wasclosed = "";
+    });
+  };
+
+  const closeAccordionsPrint = () => {
+    const accordions: NodeListOf<HTMLDetailsElement> =
+      document.body.querySelectorAll("details[data-wasclosed]");
+    accordions.forEach((e) => {
+      e.removeAttribute("open");
+      delete e.dataset.wasclosed;
+    });
   };
 
   useEffect(() => {
-    window
-      .matchMedia("print")
-      .addEventListener("change", handlePrintAccordions);
+    window.addEventListener("beforeprint", openAccordionsPrint);
+    window.addEventListener("afterprint", closeAccordionsPrint);
     return () => {
-      window
-        .matchMedia("print")
-        .removeEventListener("change", handlePrintAccordions);
+      window.removeEventListener("beforeprint", openAccordionsPrint);
+      window.removeEventListener("afterprint", closeAccordionsPrint);
     };
   }, []);
 
