f0d049d6325ebea8b26d1c0406d29e19dd01dbb5
Maxwell Austensen <m.austensen@gmail.com>
2025-05-27 16:56:12 -0400

    add not clarifying the "as of" cpi date

--- src/Components/Pages/RentCalculator/RentCalculator.tsx
+++ src/Components/Pages/RentCalculator/RentCalculator.tsx
@@ -15,6 +15,8 @@ import { gtmPush } from "../../../google-tag-manager";
 
 // This needs to be updated each year when DHCR publishes the new number
 export const CPI = 3.79;
+// This date isn't actually official, just what HPD has on their site. 
+// The law never included when the changes come into effect
 export const CPI_EFFECTIVE_DATE = "February 19, 2025";
 
 export const RentCalculator: React.FC = () => {
