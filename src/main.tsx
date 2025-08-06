73eb371ae0a5e17ef9220ebb55dbdbd6dd75d41a
Hannah Wischnia <hannahwischnia@gmail.com>
2025-08-06 12:56:08 -0400

    pin node version and add provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { ErrorBoundary, Provider as RollbarProvider } from "@rollbar/react";
 import App from "./App.tsx";
 import "./index.scss";
+import { i18n } from "@lingui/core";
+import { messages } from "./locales/en/messages";
+import { I18nProvider } from "@lingui/react";
 
 const rollbarConfig = {
   accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
@@ -12,12 +15,17 @@ const rollbarConfig = {
     import.meta.env.MODE === "production",
 };
 
+i18n.load("en", messages);
+i18n.activate("en");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RollbarProvider config={rollbarConfig}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <I18nProvider i18n={i18n}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </I18nProvider>
     </RollbarProvider>
   </StrictMode>
 );
