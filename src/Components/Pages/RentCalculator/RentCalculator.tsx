1dee4d25990d7329ca258bdd3b8ad964d823fc24
kiwansim <kiwan@justfix.org>
2025-10-08 11:35:24 -0400

    Update src/Components/Pages/RentCalculator/RentCalculator.tsx
    Co-authored-by: Maxwell Austensen <maxwell@justfix.org>

--- src/Components/Pages/RentCalculator/RentCalculator.tsx
+++ src/Components/Pages/RentCalculator/RentCalculator.tsx
@@ -76,8 +76,8 @@ export const RentCalculator: React.FC = () => {
       <PhoneNumberModal
         modalIsOpen={showPhoneModal}
         modalOnClose={handlePhoneModalClose}
-        headerText="Send the rent increase calculator to your phone"
-        bodyText="Get a text with a URL to the rent calculator"
+        headerText={_(`Send the rent increase calculator to your phone`)}
+        bodyText={_(`Get a text with a URL to the rent calculator`)}
         gtmId="rent-calculator-page-modal"
       />
       <div className="content-section">
