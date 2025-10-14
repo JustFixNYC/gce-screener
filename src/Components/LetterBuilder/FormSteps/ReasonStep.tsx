fa6a94728dbda38876a662ad910f4da8ae533b18
Maxwell Austensen <maxwell@justfix.org>
2025-10-14 07:45:28 -0700

    Add letter reason steps (#185)
    Adds step for selecting reason for the letter (planned rent increase or non renewal of lease), and a followup step to clarify the reason (if increase is above reasonable standard or if the landlord gave a good cause reason for non renewal).
    
    I also added to the "back" button to clear values from the current step before going back, this prevents an awkward situation where if you land on a page (eg user details) then go back to change answers, and when you return to user details it shows error messages right away.
    
    Unrelated, but I moved around the rent increase limit variables so exports on rent calculator script doesn't prevent fast-reloading.
    
    Eventually the steps will need to be updated with the new system for managing the flow through steps and progress bar etc.
    
    I updated the zod schema to use their discriminatedUnion method to allow for conditionally required fields based on the selected reason for the letter. This was a little awkward when working with the react-hook-form errors because it wasn't interpreting which branch you're on like is was supposed to, so I added some typescript typeguard predicate functions to fix this.
    
    There are some changes we'll need to make for the non-renewal branch, since we are talking about maybe just an intersitital, so this separate step won't be needed. I figure we can just deal with all that in a separate PR to add that once designs are ready - but let me know if you prefer me to just remove all of that now.
    
    In the process of doing this I also realized we need to fix our zod+lingui integration so that the errors messages are translated. But this PR was already getting big so I'll follow up with a separate one for that work.

---
+++ src/Components/LetterBuilder/FormSteps/ReasonStep.tsx
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import { useLingui } from "@lingui/react";
+import { msg } from "@lingui/core/macro";
+import { Trans } from "@lingui/react/macro";
+import { FormGroup, SelectButton } from "@justfixnyc/component-library";
+
+import { FormHookProps } from "../../../types/LetterFormTypes";
+import { InfoBox } from "../../InfoBox/InfoBox";
+import Modal from "../../Modal/Modal";
+import { JFCLLinkExternal } from "../../JFCLLink";
+
+export const ReasonStep: React.FC<FormHookProps> = (props) => {
+  const {
+    register,
+    formState: { errors },
+  } = props;
+
+  const { _ } = useLingui();
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <FormGroup
+        legendText={_(msg`Select the reason for your letter`)}
+        invalid={!!errors?.reason}
+        invalidText={errors?.reason?.message}
+        invalidRole="status"
+        helperElement={<ReasonHelperText onClick={() => setShowModal(true)} />}
+      >
+        <SelectButton
+          {...register("reason")}
+          labelText={_(msg`Your landlord is planning to raise your rent`)}
+          value="PLANNED_INCREASE"
+          id="reason__planned-increase"
+        />
+        <SelectButton
+          {...register("reason")}
+          labelText={_(msg`Your landlord is not offering you a new lease`)}
+          value="NON_RENEWAL"
+          id="reason__non-renewal"
+        />
+      </FormGroup>
+
+      <Modal
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+        hasCloseBtn={true}
+        header={_(msg`How to choose the reason for your letter`)}
+      >
+        <Trans>Lorem ipsum dolor sit amet</Trans>
+        <div className="callout-box">
+          <Trans>
+            You should select “
+            <strong>Your landlord has recently proposed a rent increase</strong>
+            ” if any of the following apply:
+          </Trans>
+          <ul>
+            <li>
+              <Trans>
+                Your landlord has told you your rent will be increasing
+              </Trans>
+            </li>
+            <li>
+              <Trans>
+                Your landlord has recently given you a new lease with a rent
+                increase
+              </Trans>
+            </li>
+            <li>
+              <Trans>
+                You’ve received a{" "}
+                <JFCLLinkExternal to="https://www.nycourts.gov/legacypdfs/courts/10jd/suffolk/dist/pdf/LandlordRentIncreaseofAtLeast5PercentResidental.pdf">
+                  226c notice detailing an upcoming rent increase
+                </JFCLLinkExternal>
+              </Trans>
+            </li>
+          </ul>
+        </div>
+        <div className="callout-box">
+          <Trans>
+            You should select “
+            <strong>
+              Your landlord has communicated that they do not intend to offer
+              you a new lease
+            </strong>
+            ” if any of the following apply:
+          </Trans>
+          <ul>
+            <li>
+              <Trans>
+                Your landlord has told you that you will not be offered a lease
+                renewal
+              </Trans>
+            </li>
+            <li>
+              <Trans>
+                You’ve received a{" "}
+                <JFCLLinkExternal to="https://www.nycourts.gov/legacypdfs/courts/10jd/suffolk/dist/pdf/LandlordsNoticeNotToRenewTenancyResidential.pdf">
+                  226c notice stating that your lease will not be renewed
+                </JFCLLinkExternal>
+              </Trans>
+            </li>
+            <li>
+              <Trans>
+                You’ve received a{" "}
+                <JFCLLinkExternal to="">notice of termination</JFCLLinkExternal>
+              </Trans>
+            </li>
+          </ul>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+const ReasonHelperText: React.FC<{ onClick: () => void }> = ({ onClick }) => {
+  return (
+    <InfoBox>
+      <>
+        <Trans>Need help choosing which letter is best for you?</Trans>{" "}
+        <button
+          type="button"
+          className="text-link-button jfcl-link"
+          onClick={onClick}
+        >
+          <Trans>Learn more</Trans>
+        </button>
+      </>
+    </InfoBox>
+  );
+};
--- src/types/LetterFormTypes.ts
+++ src/types/LetterFormTypes.ts
@@ -1,7 +1,9 @@
-import { UseFormReturn } from "react-hook-form";
+import { FieldErrors, UseFormReturn } from "react-hook-form";
 import z from "zod";
 
-const addressLOB = z.object({
+import { CPI } from "../Components/Pages/RentCalculator/RentIncreaseValues";
+
+const lobAddressSchema = z.object({
   primary_line: z.string(),
   secondary_line: z.string().optional(),
   city: z.string(),
@@ -10,52 +12,98 @@ const addressLOB = z.object({
     .string()
     .regex(
       /^\d{5}((-)?\d{4})?$/,
-      "ZIP Code must be of format 12345 or 12345-1234"
+      `ZIP Code must be either 5-digit number or 5-digits with hyphen and 4 additional digits.`
     ),
   urbanization: z.string().optional(),
 });
 
-export const FormSchema = z.object({
-  user_details: z.object({
-    first_name: z.string().min(1, "Name is required for the letter"),
-    last_name: z.string().min(1, "Name is required for the letter"),
-    phone_number: z
-      .string({
-        error: (iss) =>
-          iss.input === undefined || iss.input === ""
-            ? "Phone number is required for follow up"
-            : "Please enter a complete US phone number",
-      })
-      .length(10, "Please enter a complete US phone number"),
-    // Email has god default validator regex, plus alternatives, and way to
-    // include custom ({pattern: /<regex>/})
-    email: z.email({
-      // To customize error messages add a function that checks different possible
-      // issues with the field, since z.email("invalid format") would also give
-      // the same message even if empty input
+const userDetailsSchema = z.object({
+  first_name: z.string().min(1, `First name is required for the letter`),
+  last_name: z.string().min(1, `Last name is required for the letter`),
+  phone_number: z
+    .string({
       error: (iss) =>
         iss.input === undefined || iss.input === ""
-          ? "Email is required to send your copy of the letter"
-          : "Please enter a valid email",
-    }),
-    ...addressLOB
-      .omit({ urbanization: true })
-      .extend({ bbl: z.string().regex(/^\d{10}$/) }).shape,
+          ? `Phone number is required for follow up`
+          : `Please enter a complete US phone number`,
+    })
+    .length(10, `Please enter a complete US phone number`),
+  // Email has god default validator regex, plus alternatives, and way to
+  // include custom ({pattern: /<regex>/})
+  email: z.email({
+    // To customize error messages add a function that checks different possible
+    // issues with the field, since z.email("invalid format") would also give
+    // the same message even if empty input
+    error: (iss) =>
+      iss.input === undefined || iss.input === ""
+        ? `Email is required to send your copy of the letter`
+        : `Please enter a valid email`,
   }),
-  landlord_details: z.object({
-    name: z.string().min(1, "Landlord name is required"),
-    email: z.email().optional(),
-    ...addressLOB.shape,
-  }),
-  mail_choice: z.literal(
-    ["WE_WILL_MAIL", "USER_WILL_MAIL"],
-    "Please select an option for mailing the letter"
-  ),
-  email_to_landlord: z.boolean(),
-  // TODO: add fieldArray of emails (see https://react-hook-form.com/docs/usefieldarray)
+  ...lobAddressSchema
+    .omit({ urbanization: true })
+    .extend({ bbl: z.string().regex(/^\d{10}$/) }).shape,
 });
 
-export type FormFields = z.infer<typeof FormSchema>;
+const landlordDetailsSchema = z.object({
+  name: z.string().min(1, `Landlord name is required`),
+  email: z.email().optional(),
+  ...lobAddressSchema.shape,
+});
+
+const letterExtrasSchema = z.object({
+  mail_choice: z.literal(
+    ["WE_WILL_MAIL", "USER_WILL_MAIL"],
+    `Please select an option for mailing the letter`
+  ),
+  email_to_landlord: z.boolean(),
+});
+
+const plannedIncreaseLetterSchema = z.object({
+  user_details: userDetailsSchema,
+  landlord_details: landlordDetailsSchema,
+  ...letterExtrasSchema.shape,
+  reason: z.literal("PLANNED_INCREASE"),
+  unreasonable_increase: z.boolean(
+    `Please select whether the increase is beyond ${CPI + 5}%`
+  ),
+});
+
+const nonRenewalLetterSchema = z.object({
+  user_details: userDetailsSchema,
+  landlord_details: landlordDetailsSchema,
+  ...letterExtrasSchema.shape,
+  reason: z.literal("NON_RENEWAL"),
+  good_cause_given: z.boolean(
+    `Please select whether your landlord provided one of the listed reasons for not renewing your lease.`
+  ),
+});
+
+export const formSchema = z.discriminatedUnion(
+  "reason",
+  [plannedIncreaseLetterSchema, nonRenewalLetterSchema],
+  { error: `Please select a reason for your letter.` }
+);
+
+export type FormFields = z.infer<typeof formSchema>;
+
+type PlanedIncreaseErrors = FieldErrors<
+  z.infer<typeof plannedIncreaseLetterSchema>
+>;
+type NonRenewalErrors = FieldErrors<z.infer<typeof nonRenewalLetterSchema>>;
+
+export function isPlannedIncreaseErrors(
+  _errors: PlanedIncreaseErrors | NonRenewalErrors,
+  reason: FormFields["reason"]
+): _errors is PlanedIncreaseErrors {
+  return reason === "PLANNED_INCREASE";
+}
+
+export function isNonRenewalErrors(
+  _errors: PlanedIncreaseErrors | NonRenewalErrors,
+  reason: FormFields["reason"]
+): _errors is NonRenewalErrors {
+  return reason === "NON_RENEWAL";
+}
 
 export type FormHookProps = UseFormReturn<FormFields>;
 
@@ -83,6 +131,8 @@ export const defaultFormValues: FormFields = {
     urbanization: undefined,
   },
   mail_choice: "WE_WILL_MAIL",
+  reason: "PLANNED_INCREASE",
+  unreasonable_increase: false,
   email_to_landlord: true,
 };
 
@@ -109,5 +159,7 @@ export const sampleFormValues: FormFields = {
     zip_code: "11111",
   },
   mail_choice: "WE_WILL_MAIL",
+  reason: "PLANNED_INCREASE",
+  unreasonable_increase: false,
   email_to_landlord: true,
 };
