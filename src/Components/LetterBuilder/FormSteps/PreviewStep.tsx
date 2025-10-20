2c4a0b174b4e04150b5c037b31ca06375afcc039
Maxwell Austensen <m.austensen@gmail.com>
2025-10-20 17:19:33 -0400

    add letter copy/layout, unify styles, add test sender

--- src/Components/LetterBuilder/FormSteps/PreviewStep.tsx
+++ src/Components/LetterBuilder/FormSteps/PreviewStep.tsx
@@ -1,17 +1,76 @@
+import { useEffect, useState } from "react";
+import { useLingui } from "@lingui/react";
 import { Trans } from "@lingui/react/macro";
+import { msg } from "@lingui/core/macro";
 
 import { FormHookProps } from "../../../types/LetterFormTypes";
-import { LetterPreview } from "../Letter/Letter";
+import { languageNames, SupportedLocale } from "../../../i18n-base";
+import { buildLetterHtml } from "../Letter/letter-utils";
 
 export const PreviewStep: React.FC<FormHookProps> = (props) => {
   const { getValues } = props;
 
+  const { i18n, _ } = useLingui();
+  const letterData = getValues();
+
+  const [letter, setLetter] = useState<string | undefined>();
+  const [previewLocale, setPreviewLocale] = useState<SupportedLocale>(
+    i18n.locale as SupportedLocale
+  );
+
+  useEffect(() => {
+    const prepLetter = async () => {
+      const localizedLetterHtml = await buildLetterHtml(
+        letterData,
+        previewLocale,
+        false
+      );
+      setLetter(localizedLetterHtml);
+    };
+    prepLetter();
+  }, [letterData, previewLocale]);
+
   return (
     <>
-      <Trans>
-        Please review your letter to ensure all the details are correct.
-      </Trans>
-      <LetterPreview letterData={getValues()} />
+      <h4>
+        <Trans>Review your letter</Trans>
+      </h4>
+      <p>
+        <Trans>
+          Make sure the information below is accurate. If it is not, you can go
+          back to make changes.
+        </Trans>
+      </p>
+
+      <div className="locale-toggle">
+        <Trans>Preview language:</Trans>
+        <button
+          className="jfcl-link"
+          onClick={() => setPreviewLocale("es")}
+          disabled={previewLocale === "es"}
+        >
+          {languageNames["es"]}
+        </button>
+        <button
+          className="jfcl-link"
+          onClick={() => setPreviewLocale("en")}
+          disabled={previewLocale === "en"}
+        >
+          {languageNames["en"]}
+        </button>
+      </div>
+      <iframe
+        title={_(msg`Good Cause Letter Preview`)}
+        srcDoc={letter}
+        width="600"
+        height="600"
+      />
+      <p>
+        <Trans>
+          <strong>Note:</strong> You will be able to download a PDF version of
+          this letter later.
+        </Trans>
+      </p>
     </>
   );
 };
