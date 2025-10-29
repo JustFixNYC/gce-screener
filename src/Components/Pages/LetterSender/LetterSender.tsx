930668690ec8c2fa18e54c6e919e88ed3234ad8d
Maxwell Austensen <maxwell@justfix.org>
2025-10-29 13:39:45 -0400

    add letter landing page (#197)
    Adds the landing page for the letter builder.
    
    For now I've updated the routes to use /letter for the landing page and /letter/build for the steps, but I think we'll be updating to have separate routes for each step latter with the progress bar/step management PR.
    
    This also updates JFCL for the change to Font Awesome v7 to get some new icons including the mailbox/envelope one used for the main CTA here. As part of the JFCL change we removed the type prop for <Icon> instead adding the type as a suffix to icon names in the existing icon prop. For details see JustFixNYC/component-library#58
    
    Now that #198 has been merged in, adding routes fro the letter steps, I've made some changes to fit in landing page with the new routes. So now /letter is the landing page, and the CTA to begin the survey navigates to /letter/reason. And I removed the header from since it's not in the current mocks and it also didn't work with the nested route/ system when adding the landing page. This is mostly just to get this PR merged in now, and we can sort out any changes to the routing later maybe when we prevent the direct linking.
    
    Like with everything else I've left out the styles from this page as well.

---
+++ src/Components/Pages/LetterSender/LetterSender.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Trans } from "@lingui/react/macro";
+import { useLingui } from "@lingui/react";
+import { msg } from "@lingui/core/macro";
+import { Button } from "@justfixnyc/component-library";
+
+import { LetterBuilderForm } from "../../LetterBuilder/LetterBuilderForm";
+import { FormFields, sampleFormValues } from "../../../types/LetterFormTypes";
+import { buildLetterHtml } from "../../LetterBuilder/Letter/letter-utils";
+import { GCELetterPostData } from "../../../types/APIDataTypes";
+import { useSendGceLetterData } from "../../../api/hooks";
+import { languageNames, SupportedLocale } from "../../../i18n-base";
+import "./LetterSender.scss";
+
+export const LetterLayout: React.FC = () => {
+  return (
+    <div id="letter-sender-page">
+      <Outlet />
+    </div>
+  );
+};
+
+export const LetterSender: React.FC = () => {
+  return (
+    <div className="content-section">
+      <div className="content-section__content">
+        <LetterTester letterData={sampleFormValues} />
+        <LetterBuilderForm />
+      </div>
+    </div>
+  );
+};
+
+// This is just to help with testing letter and backend so you don't have to
+// complete all form steps each time. This should all be removed later
+export const LetterTester: React.FC<{ letterData: FormFields }> = ({
+  letterData,
+}) => {
+  const { i18n, _ } = useLingui();
+
+  const { trigger: sendLetter } = useSendGceLetterData();
+
+  const [letter, setLetter] = useState<string | undefined>();
+  const [previewLocale, setPreviewLocale] = useState<SupportedLocale>(
+    i18n.locale as SupportedLocale
+  );
+
+  useEffect(() => {
+    const prepLetter = async () => {
+      const localizedLetterHtml = await buildLetterHtml(
+        letterData,
+        previewLocale
+      );
+      setLetter(localizedLetterHtml);
+    };
+    prepLetter();
+  }, [letterData, previewLocale]);
+
+  return (
+    <>
+      <div className="locale-toggle">
+        <Trans>Preview language:</Trans>
+        <button
+          className="jfcl-link"
+          onClick={() => setPreviewLocale("es")}
+          disabled={previewLocale === "es"}
+        >
+          {languageNames["es"]}
+        </button>
+        <button
+          className="jfcl-link"
+          onClick={() => setPreviewLocale("en")}
+          disabled={previewLocale === "en"}
+        >
+          {languageNames["en"]}
+        </button>
+      </div>
+
+      <iframe
+        title={_(msg`Good Cause Letter Preview`)}
+        srcDoc={letter}
+        width="600"
+        height="600"
+      />
+
+      <Button
+        labelText="submit letter"
+        onClick={async () => {
+          const letterHtml = await buildLetterHtml(sampleFormValues, "en");
+          const letterPostData: GCELetterPostData = {
+            ...sampleFormValues,
+            extra_emails: letterData.extra_emails
+              ?.map(({ email }) => email)
+              .filter((email): email is string => !!email),
+            html_content: letterHtml,
+          };
+          await sendLetter(letterPostData);
+        }}
+      />
+    </>
+  );
+};
