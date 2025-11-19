845dccbc7d0d27d59c4a95d19c2e59c5ca2ebf26
Maxwell Austensen <maxwell@justfix.org>
2025-11-19 17:30:22 -0500

    Update src/Components/LetterBuilder/LetterNextSteps/LetterNextSteps.tsx

--- src/Components/LetterBuilder/LetterNextSteps/LetterNextSteps.tsx
+++ src/Components/LetterBuilder/LetterNextSteps/LetterNextSteps.tsx
@@ -302,8 +302,8 @@ export const ClaimsNotCoveredByGCE: React.FC<NextStepItemProps> = (props) => (
         <Trans>What to know</Trans>
       </h5>
       <p>
-        Coverage depends on your building type, subsidy status, and other
-        factors.
+        <Trans>Coverage depends on your building type, subsidy status, and other
+factors.</Trans>
       </p>
     </section>
     <section>
