4fc30bef37508da5c4142acd3f8beae8e48fae3b
Maxwell Austensen <m.austensen@gmail.com>
2025-11-24 12:24:49 -0500

    update fixed header show/hide on scroll

--- src/Components/TopBar/TopBar.tsx
+++ src/Components/TopBar/TopBar.tsx
@@ -5,62 +5,56 @@ import { Trans } from "@lingui/react/macro";
 import { useLingui } from "@lingui/react";
 
 import { gtmPush } from "../../google-tag-manager";
-import { useScrollDirection } from "../../hooks/useScrollDirection";
-import useInViewPort from "../../hooks/useInViewport";
+import { useHideHeader } from "../../hooks/useHideHeader";
 import { JFCLLinkInternal } from "../JFCLLink";
 import "./TopBar.scss";
 
 export const TopBar: React.FC = () => {
   const { i18n } = useLingui();
-  const isScrollingUp = useScrollDirection() === "up";
-  const placeholderRef = useRef<HTMLDivElement | null>(null);
-  const placeholderInViewport = useInViewPort(placeholderRef);
-  const hideTopbar = !(isScrollingUp || placeholderInViewport);
+  const headerRef = useRef<HTMLElement>(null);
+  const hideHeader = useHideHeader(headerRef);
 
   return (
-    <>
-      <header id="topbar" className={classNames(hideTopbar && "hide")}>
-        <div className="topbar__name">
-          <h1>
-            <Link to={`/${i18n.locale}`}>
-              <Trans>Good Cause NYC</Trans>
-            </Link>
-          </h1>
-        </div>
-        <div className="topbar__collab">
-          <Trans>
-            <span>By</span>{" "}
-            <a
-              href="https://housingjusticeforall.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Housing Justice for All
-            </a>{" "}
-            <span>&</span>{" "}
-            <a
-              href="https://justfix.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              JustFix
-            </a>
-          </Trans>
-        </div>
-        <div className="topbar__rent-calculator">
-          <JFCLLinkInternal
-            to={`/${i18n.locale}/rent_calculator`}
-            onClick={() => gtmPush("gce_rent_calculator", { from: "navbar" })}
+    <header
+      ref={headerRef}
+      id="topbar"
+      className={classNames({ hide: hideHeader })}
+    >
+      <div className="topbar__name">
+        <h1>
+          <Link to={`/${i18n.locale}`}>
+            <Trans>Good Cause NYC</Trans>
+          </Link>
+        </h1>
+      </div>
+      <div className="topbar__collab">
+        <Trans>
+          <span>By</span>{" "}
+          <a
+            href="https://housingjusticeforall.org/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <Trans>Rent increase calculator</Trans>
-          </JFCLLinkInternal>
-        </div>
-      </header>
-      <div
-        ref={placeholderRef}
-        id="topbar-placeholder"
-        className={classNames(!hideTopbar && "hide")}
-      />
-    </>
+            Housing Justice for All
+          </a>{" "}
+          <span>&</span>{" "}
+          <a
+            href="https://justfix.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            JustFix
+          </a>
+        </Trans>
+      </div>
+      <div className="topbar__rent-calculator">
+        <JFCLLinkInternal
+          to={`/${i18n.locale}/rent_calculator`}
+          onClick={() => gtmPush("gce_rent_calculator", { from: "navbar" })}
+        >
+          <Trans>Rent increase calculator</Trans>
+        </JFCLLinkInternal>
+      </div>
+    </header>
   );
 };
---
+++ src/hooks/useHideHeader.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+/** Hook to get value for whether to hide top nav fixed header */
+// https://stackoverflow.com/a/62497293/7051239
+// https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c
+export const useHideHeader = (
+  headerRef: React.RefObject<HTMLElement>,
+  threshold: number = 10
+): boolean => {
+  const [hideHeader, setHideHeader] = useState<boolean>(true);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY || window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.scrollY || window.pageYOffset;
+
+      // Prevents possible jitter from tiny scroll changes
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+
+      const headerHeight = headerRef?.current?.offsetHeight;
+
+      if (headerHeight && scrollY < headerHeight) {
+        // prevents seeing "behind" the header
+        setHideHeader(false);
+      } else {
+        setHideHeader(scrollY > lastScrollY);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [headerRef, threshold]);
+
+  return hideHeader;
+};
