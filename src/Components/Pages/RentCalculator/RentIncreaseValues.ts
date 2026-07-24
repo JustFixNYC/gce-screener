4e464fb1843a0f29ab44c08881f68efbde9c7ce2
Maxwell Austensen <maxwell@justfix.org>
2026-07-24 11:11:36 -0400

    update CPI and rent levels for new year (#298)
    DHCR has now upblished the new CPI and HUD FMR rents in this notice: https://hcr.ny.gov/system/files/documents/2026/07/gce-fact-sheet-05.04.26-update.pdf
    
    It is dated "as of" may 4 2026, so is retroactive.
    
    In a future PR we'll add some way to communicate all of the past years' rent increase percentages.

--- src/Components/Pages/RentCalculator/RentIncreaseValues.ts
+++ src/Components/Pages/RentCalculator/RentIncreaseValues.ts
@@ -1,7 +1,6 @@
 import { msg } from "@lingui/core/macro";
 
 // This needs to be updated each year when DHCR publishes the new number
-export const CPI = 3.79;
-// This date isn't actually official, just what HPD has on their site.
-// The law never included when the changes come into effect
-export const CPI_EFFECTIVE_DATE = msg`February 18, 2025`;
+export const CPI = 3.38;
+// The "as of" date from the DHCR notice. This is retroactive this year.
+export const CPI_EFFECTIVE_DATE = msg`May 4, 2026`;
